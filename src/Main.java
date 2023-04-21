1aa1769c9f1b96d0da7e769c4a9d7773496485c4
fkj <fkj@eamv.dk>
2023-04-21 11:02:36 +0200

    Har lavet testanvendelse af Person-klassen, i Main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    Person p1 = new Person("Anders And");
+    
+    System.out.println(p1);
   }
 }
